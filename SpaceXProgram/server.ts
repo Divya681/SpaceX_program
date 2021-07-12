0288486f0ad396ae2db1b394d577a9d076ae0f78
Divya681 <divyagoyal681@gmail.com>
2021-07-12 22:11:32 +0530

    heroku deploy

--- SpaceXProgram/server.ts
+++ SpaceXProgram/server.ts
@@ -1,16 +1,41 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+import 'zone.js/dist/zone-node';
 
+import * as express from 'express';
+import {join} from 'path';
+
+// Express server
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SpaceXProgram/browser'));
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
-app.get('/*', function(req:any,res:any) {
-    
-res.sendFile(path.join(__dirname+'/dist/SpaceXProgram/browser/index.html'));
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
+
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
+// Serve static files from /browser
+app.get('*.*', express.static(DIST_FOLDER, {
+  maxAge: '1y'
+}));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render('index', { req });
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
