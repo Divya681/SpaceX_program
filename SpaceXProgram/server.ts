bb41f0199be9092f0c6367cf811212e81cc40f30
Divya681 <divyagoyal681@gmail.com>
2021-07-12 22:03:23 +0530

    heroku deploy

--- SpaceXProgram/server.ts
+++ SpaceXProgram/server.ts
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/SpaceXProgram/browser'));
+app.use(express.static(__dirname + '/dist/SpaceXProgram/browser'));
 
-app.get('/*', (req:any, res:any) =>
-    res.sendFile('index.html', {root: 'dist/SpaceXProgram/browser/index.html'}),
-);
+app.get('/*', function(req:any,res:any) {
+    
+res.sendFile(path.join(__dirname+'/dist/SpaceXProgram/browser/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
