7a5bca64cc8a614947e0eded5d4b2ef17837d6fb
Divya681 <divyagoyal681@gmail.com>
2021-07-13 03:07:48 +0530

    changes

--- server.js
+++ server.js
@@ -1,41 +1,8 @@
-//import 'zone.js/dist/zone-node';
-
 const express = require('express');
 const path = require('path');
-
-// Express server
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = path.join(process.cwd(), 'dist/SpaceXProgram/browser');
-
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-//const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-// app.engine('html', ngExpressEngine({
-//   bootstrap: AppServerModuleNgFactory,
-//   providers: [
-//     provideModuleMap(LAZY_MODULE_MAP)
-//   ]
-// }));
-
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
-
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
-// Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
+const ngApp = express();
+ngApp.use(express.static('./dist/SpaceXProgram/browser'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/SpaceXProgram/browser/index.html'));
 });
-
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+ngApp.listen(process.env.PORT || 8080);
