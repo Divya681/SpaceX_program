924a1775a520f6cad3cebf4f8beeed51dc4ac8ce
Divya681 <divyagoyal681@gmail.com>
2021-07-12 21:58:02 +0530

    heroku commit server.ts fix

--- SpaceXProgram/server.ts
+++ SpaceXProgram/server.ts
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/SpaceXProgram'));
+app.use(express.static('./dist/SpaceXProgram/browser'));
 
 app.get('/*', (req:any, res:any) =>
     res.sendFile('index.html', {root: 'dist/SpaceXProgram/browser/index.html'}),
